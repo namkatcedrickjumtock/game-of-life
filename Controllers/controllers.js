f48cc7052a50956eb1098481a97465e8a40c4f8f
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-06-29 10:34:48 +0100

    crown's game of life

---
+++ Controllers/controllers.js
@@ -0,0 +1,228 @@
+
+
+// ``
+//create array of iDs from 1 to numOfCells//___________________________________________________________________________________________
+let id = 1;
+let whileTrack = 0
+let cellIds = []
+let numOfRows = 50;
+let numOfRowsCurr = numOfRows + 2;
+let numOfCells = numOfRows**2
+let numOfCellsCurr = numOfRowsCurr**2
+
+for(let i = 0; i < numOfRows;i++){
+    for(let j = 0; j < numOfRows; j++){
+        cellIds.push(id);
+        id++
+    }
+    
+}
+//Dynamicaly create numOfRows by numOfRows table
+console.log(cellIds)
+let cells = ""
+for(let i = 0; i < numOfRows;i++){
+    
+   let tbody = document.getElementById("tbody")//to bve refactored
+    tbody.innerHTML += "<tr>" + "<td></td>".repeat(numOfRows) + "</tr>"
+}
+let tbody = document.getElementById("tbody")
+// let arr = body.children;
+// console.log(arr)
+function delay(time){
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+let tempid = 1;
+for(let i = 0; i < numOfRows;i++){
+    for(let j = 0; j < numOfRows;j++){
+        let cell = tbody.children[i].children[j]
+cell.style.backgroundColor = "green"
+cell.setAttribute("id", tempid)//Assign ID dynamically from 1 to numOfCells
+
+//create click even for each cell______________________________________________________________________________
+const btn = document.getElementById(tempid);
+
+btn.addEventListener('click', function onClick(event) {
+  // 👇️ Change cell color switching between two colors
+  if(cell.style.backgroundColor == 'yellow'){
+    cell.style.backgroundColor = 'green'
+  }else if(cell.style.backgroundColor == 'green'){
+    cell.style.backgroundColor = 'yellow'
+  }
+});
+//________________________________________________________
+tempid++;
+}
+}
+
+//Create Actions for start button______________________________________________________________________________
+const start = document.getElementById("start_btn");
+
+start.addEventListener('click', function onClick(event) {
+    // let cellCurrStates = []
+    // console.log("line 78 cell current state", cellCurrStates)
+    // let cellNextStates = []
+    
+    // console.log("line 80 cell next state", cellNextStates)
+    let num = 1;
+    let counter = 1
+    whileTrack = 0
+   
+    function automateClick(){setTimeout(function(){
+        let cellCurrStates = []
+    console.log("line 78 cell current state", cellCurrStates)
+    let cellNextStates = []
+    
+    console.log("line 80 cell next state", cellNextStates)
+
+// instantiate tracking arrays to track current and next states
+    for(let i = 0; i < numOfCellsCurr; i++){
+        cellCurrStates.push(0)
+        cellNextStates.push(0)
+    }
+
+
+     for(let i = 1; i <= numOfCells;i++){
+    
+    let temp = document.getElementById(i)
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    if(temp.style.backgroundColor == "green"){
+        cellCurrStates[trackCurrState] = 0
+    }else{
+        cellCurrStates[trackCurrState] = 1
+    }
+}
+//_________________________________________tracking current states
+
+//setting next states from current states following rules//_________________________________________
+
+for(let i = 1; i <= numOfCells;i++){
+    let temp = document.getElementById(i)
+    //still to account for cell at the edges
+    //originally off state____________________________________________________________________________________________
+
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    let surrCellState = (
+        cellCurrStates[(trackCurrState) - (numOfRowsCurr + 1) ] + cellCurrStates[(trackCurrState) - numOfRowsCurr] + cellCurrStates[(trackCurrState)-(numOfRowsCurr - 1)] + cellCurrStates[(trackCurrState)-1] + 
+        cellCurrStates[(trackCurrState)+1] + cellCurrStates[(trackCurrState)+(numOfRowsCurr - 1)] + cellCurrStates[(trackCurrState)+numOfRowsCurr] + cellCurrStates[(trackCurrState)+(numOfRowsCurr + 1)]
+    )
+
+    if(cellCurrStates[trackCurrState] == 0 && surrCellState == 3){
+        cellNextStates[trackCurrState] = 1
+    }
+    //Any live cell with fewer than two live neighbours dies (referred to as underpopulation).//________________________________
+    else if(cellCurrStates[trackCurrState] == 1 && surrCellState < 2){
+        cellNextStates[trackCurrState] = 0
+    }
+    //Any live cell with more than three live neighbours dies (referred to as overpopulation).//________________________________
+    else if(cellCurrStates[trackCurrState] == 1 && surrCellState > 3){
+        cellNextStates[trackCurrState] = 0
+    }
+    //Any live cell with two or three live neighbours lives, unchanged, to the next generation.//______________________________
+    //refactor the suming
+    else if(cellCurrStates[trackCurrState] == 1 && surrCellState == 3){
+        cellNextStates[trackCurrState] = 1
+    }else if(cellCurrStates[trackCurrState] == 1 && surrCellState == 2){
+        cellNextStates[trackCurrState] = 1
+    }
+    //__________________________________________________________________________________________________________________________
+    else(
+        cellNextStates[trackCurrState] = 0
+    )
+    //___________________________________________________________________________________________________________________
+}
+//using Next state to change collor(applying state)
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    let temp = document.getElementById(i)
+    if(cellNextStates[trackCurrState] == 1){
+        temp.style.backgroundColor = "yellow"
+    }else{
+        temp.style.backgroundColor = "green"
+    }
+}
+
+    // console.log(num)
+    // num++
+    // counter++
+    // cellCurrStates = [...cellNextStates]
+    console.log("line 156 cell curr state", cellCurrStates)
+    console.log("line 156 cell next state", cellNextStates)
+    if(whileTrack == 1){
+        return 0
+    }
+    automateClick()
+    }, 100)}
+    automateClick()
+});
+
+//________________________________________________________
+
+//Math.floor(Math.random() * (max - min) + min);
+//random Action//_________________________________________________
+const random = document.getElementById("random_btn");
+
+random.addEventListener('click', function onClick(event) {
+let cellCurrStates = []
+let cellNextStates = []
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    cellNextStates[trackCurrState] = (Math.floor(Math.random() * (3 - 0) + 0))
+}
+//using Next state to change collor(applying state)
+for(let i = 1; i <= numOfCells;i++){
+    let temp = document.getElementById(i)
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    if(cellNextStates[trackCurrState] == 1){
+        temp.style.backgroundColor = "yellow"
+    }else{
+        temp.style.backgroundColor = "green"
+    }
+}
+    //console.log(cellCurrStates)
+});
+
+//reset Actions//________________________________________________
+const reset = document.getElementById("reset_btn");
+
+reset.addEventListener('click', function onClick(event) {
+let cellCurrStates = []
+let cellNextStates = []
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    cellNextStates[trackCurrState] = 0
+}
+//using Next state to change collor(applying state)
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    let temp = document.getElementById(i)
+    if(cellNextStates[trackCurrState] == 1){
+        temp.style.backgroundColor = "yellow"
+    }else{
+        temp.style.backgroundColor = "green"
+    }
+}
+    //console.log(cellCurrStates)
+    whileTrack = 1
+});
+const stopB = document.getElementById("stop_btn");
+
+stopB.addEventListener('click', function onClick(event) {
+// let cellCurrStates = []
+// let cellNextStates = []
+// for(let i = 1; i <= numOfCells;i++){
+//     let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+//     cellNextStates[trackCurrState] = 0
+// }
+//using Next state to change collor(applying state)
+// for(let i = 1; i <= numOfCells;i++){
+//     let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+//     let temp = document.getElementById(i)
+//     if(cellNextStates[trackCurrState] == 1){
+//         temp.style.backgroundColor = "yellow"
+//     }else{
+//         temp.style.backgroundColor = "green"
+//     }
+// }
+    //console.log(cellCurrStates)
+    whileTrack = 1
+});
---
+++ activity Diagram.io
@@ -0,0 +1,54 @@
+<mxfile host="65bd71144e">
+    <diagram id="PL4WEfl3c0OADvd_Qpcb" name="Page-1">
+        <mxGraphModel dx="1491" dy="1945" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
+            <root>
+                <mxCell id="0"/>
+                <mxCell id="1" parent="0"/>
+                <mxCell id="25" style="edgeStyle=none;html=1;exitX=0.5;exitY=1;exitDx=0;exitDy=0;fontColor=#FFFFFF;" edge="1" parent="1" source="2">
+                    <mxGeometry relative="1" as="geometry">
+                        <mxPoint x="440" y="230" as="targetPoint"/>
+                    </mxGeometry>
+                </mxCell>
+                <mxCell id="26" style="edgeStyle=none;html=1;exitX=0;exitY=1;exitDx=0;exitDy=0;fontColor=#FFFFFF;" edge="1" parent="1" source="2">
+                    <mxGeometry relative="1" as="geometry">
+                        <mxPoint x="290" y="230" as="targetPoint"/>
+                    </mxGeometry>
+                </mxCell>
+                <mxCell id="27" style="edgeStyle=none;html=1;exitX=1;exitY=1;exitDx=0;exitDy=0;fontColor=#FFFFFF;" edge="1" parent="1" source="2">
+                    <mxGeometry relative="1" as="geometry">
+                        <mxPoint x="600" y="230" as="targetPoint"/>
+                    </mxGeometry>
+                </mxCell>
+                <mxCell id="28" style="edgeStyle=none;html=1;exitX=1;exitY=0.5;exitDx=0;exitDy=0;fontColor=#FFFFFF;" edge="1" parent="1" source="2">
+                    <mxGeometry relative="1" as="geometry">
+                        <mxPoint x="750" y="230" as="targetPoint"/>
+                    </mxGeometry>
+                </mxCell>
+                <mxCell id="2" value="Start" style="ellipse;fillColor=#ffffff;strokeColor=#000000;fontColor=#000000;" vertex="1" parent="1">
+                    <mxGeometry x="485" y="6.25" width="75" height="62.5" as="geometry"/>
+                </mxCell>
+                <mxCell id="5" value="&lt;font color=&quot;#ffffff&quot;&gt;Start Automat&lt;/font&gt;" style="rounded=1;whiteSpace=wrap;html=1;fontColor=#000000;" vertex="1" parent="1">
+                    <mxGeometry x="450" y="400" width="120" height="40" as="geometry"/>
+                </mxCell>
+                <mxCell id="8" value="&lt;font color=&quot;#ffffff&quot;&gt;Reset?&lt;/font&gt;" style="rhombus;whiteSpace=wrap;html=1;fontColor=#000000;dashed=1;rounded=1;strokeWidth=3;" vertex="1" parent="1">
+                    <mxGeometry x="560" y="240" width="80" height="80" as="geometry"/>
+                </mxCell>
+                <mxCell id="10" value="&lt;font color=&quot;#ffffff&quot;&gt;isRandom&lt;br&gt;?&lt;/font&gt;" style="rhombus;whiteSpace=wrap;html=1;fontColor=#000000;dashed=1;rounded=1;strokeWidth=3;" vertex="1" parent="1">
+                    <mxGeometry x="395" y="240" width="80" height="80" as="geometry"/>
+                </mxCell>
+                <mxCell id="11" value="&lt;font color=&quot;#ffffff&quot;&gt;onClick?&lt;/font&gt;" style="rhombus;whiteSpace=wrap;html=1;fontColor=#000000;dashed=1;rounded=1;strokeWidth=3;" vertex="1" parent="1">
+                    <mxGeometry x="240" y="240" width="80" height="80" as="geometry"/>
+                </mxCell>
+                <mxCell id="20" value="" style="edgeStyle=none;html=1;fontColor=#FFFFFF;" edge="1" parent="1" source="19" target="2">
+                    <mxGeometry relative="1" as="geometry"/>
+                </mxCell>
+                <mxCell id="19" value="&lt;font color=&quot;#ffffff&quot;&gt;When Resources loads&lt;/font&gt;" style="shape=note;whiteSpace=wrap;html=1;backgroundOutline=1;darkOpacity=0.05;rounded=1;fontColor=#000000;strokeWidth=1;size=2;" vertex="1" parent="1">
+                    <mxGeometry x="240" y="-10" width="76" height="95" as="geometry"/>
+                </mxCell>
+                <mxCell id="24" value="&lt;font color=&quot;#ffffff&quot;&gt;stop&lt;/font&gt;" style="rhombus;whiteSpace=wrap;html=1;fontColor=#000000;dashed=1;rounded=1;strokeWidth=3;" vertex="1" parent="1">
+                    <mxGeometry x="720" y="240" width="80" height="80" as="geometry"/>
+                </mxCell>
+            </root>
+        </mxGraphModel>
+    </diagram>
+</mxfile>
